6327dcfd3232afcf995b500d00f66827bce1fce6
negineidgahi <negin.eidgahi68@thehutgroup.com>
2023-01-09 14:46:49 +0000

    first entry

--- src/main/java/com/thg/accelerator23/connectn/ai/girls/NeginZivaConnector.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/girls/NeginZivaConnector.java
@@ -1,20 +1,23 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.girls;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.Random;
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+
+public class NeginZivaConnector extends Player {
+  public NeginZivaConnector(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, NeginZivaConnector.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
+
     return 4;
   }
 }
